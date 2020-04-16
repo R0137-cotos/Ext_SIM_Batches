6e4623b337df1e69769018387eef5a4b8179cb0a
YamazakiTakumi <yamazaki-takumi@softcomm.co.jp>
2020-04-16 10:48:11 +0900

    SendOrderMailコピーしてSendDeviceBlankAlertMail作成

---
+++ Communication/SendDeviceBlankAlertMail/src/main/java/jp/co/ricoh/cotos/logic/BatchComponent.java
@@ -0,0 +1,44 @@
+package jp.co.ricoh.cotos.logic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import jp.co.ricoh.cotos.component.IBatchStepComponent;
+import jp.co.ricoh.cotos.component.base.BatchStepComponent;
+import jp.co.ricoh.cotos.dto.SendOrderMailDto;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class BatchComponent {
+
+	private final ApplicationContext context;
+
+	@Autowired
+	@Qualifier("BASE")
+	BatchStepComponent baseComponent;
+
+	/**
+	 * バッチ処理
+	 * @return
+	 * @throws Exception 
+	 */
+	public void execute(String[] args) throws Exception {
+		// パラメータチェック
+		SendOrderMailDto dto = baseComponent.paramCheck(args);
+
+		IBatchStepComponent component = this.getComponentInstance("SIM");
+		component.process(dto);
+	}
+
+	/**
+	 * 商材切替
+	 * @param productDiv
+	 * @return IBatchStepComponent
+	 */
+	public IBatchStepComponent getComponentInstance(String productDiv) {
+		return (IBatchStepComponent) context.getBean("iBatchStepComponent", productDiv);
+	}
+}
