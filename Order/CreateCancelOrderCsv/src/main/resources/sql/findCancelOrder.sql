915dc55998afb5ae2aa561675d4031a4b18b87bf
sakurada keigo <sakurada-keigo@softcomm.co.jp>
2020-07-14 17:21:30 +0900

    #7519 解約手配CSV作成 合計数量16を超える契約も対象データとして検知可能にする修正

--- Order/CreateCancelOrderCsv/src/main/resources/sql/findCancelOrder.sql
+++ Order/CreateCancelOrderCsv/src/main/resources/sql/findCancelOrder.sql
@@ -33,7 +33,7 @@ WHERE
     c.lifecycle_status = '2'
     AND c.workflow_status = '3'
     AND c.contract_type = '2'
-    AND JSON_EXISTS(JSON_QUERY(pc.EXTENDS_PARAMETER_ITERANCE, '$.extendsParameterList'), '$?(@.contractType == "解約")')
+    AND JSON_EXISTS(pc.EXTENDS_PARAMETER_ITERANCE, '$.extendsParameterList?(@.contractType == "解約")')
   ) 
 ORDER BY
   c.id
