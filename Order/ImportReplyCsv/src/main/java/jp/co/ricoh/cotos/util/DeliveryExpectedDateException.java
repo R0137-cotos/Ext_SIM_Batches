f857ed9bc8b355ac1fdc437f957b55c1f4afc488
sakurada keigo <sakurada-keigo@softcomm.co.jp>
2020-07-21 11:31:39 +0900

    #7583 リプライCSV取込 CSVに納入予定日(サービス開始日)が入っていない状態でリプライCSV取込を実施した際にエラーログを出力する

---
+++ Order/ImportReplyCsv/src/main/java/jp/co/ricoh/cotos/util/DeliveryExpectedDateException.java
@@ -0,0 +1,14 @@
+package jp.co.ricoh.cotos.util;
+
+public class DeliveryExpectedDateException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DeliveryExpectedDateException() {
+		super();
+	}
+
+	public DeliveryExpectedDateException(Throwable e) {
+		super(e);
+	}
+}
