3e74fd3a9fc67986fcfe3f50716a3bf6ed96bb54
YamazakiTakumi <yamazaki-takumi@softcomm.co.jp>
2021-01-28 14:03:02 +0900

    佐川コードCSV読み込みに修正

--- Order/CreateOrderCsv/src/main/java/jp/co/ricoh/cotos/component/BatchUtil.java
+++ Order/CreateOrderCsv/src/main/java/jp/co/ricoh/cotos/component/BatchUtil.java
@@ -2,28 +2,32 @@ package jp.co.ricoh.cotos.component;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 
 import jp.co.ricoh.cotos.commonlib.entity.contract.ContractInstallationLocation;
+import jp.co.ricoh.cotos.commonlib.exception.ErrorCheckException;
+import jp.co.ricoh.cotos.commonlib.exception.ErrorInfo;
 import jp.co.ricoh.cotos.commonlib.logic.check.CheckUtil;
 import jp.co.ricoh.cotos.commonlib.repository.contract.ContractInstallationLocationRepository;
+import jp.co.ricoh.cotos.dto.SagawaCodeDto;
 
 @Component
 public class BatchUtil {
@@ -111,49 +115,25 @@ public class BatchUtil {
 	 * @return
 	 */
 	public String getSagawaCodeColumnF(List<String> postNumberList) {
-		InputStream in = null;
-		Workbook wb = null;
-
-		// エクセルファイルを読み込む
-		Exception error = null;
+		// 佐川コード突き当てCSV読込
 		try {
-			in = this.getClass().getClassLoader().getResourceAsStream("file/佐川コード突き当て.xlsx");
-			wb = WorkbookFactory.create(in);
-		} catch (IOException e) {
-			e.printStackTrace();
-			error = e;
-		} catch (InvalidFormatException e) {
-			e.printStackTrace();
-			error = e;
-		} finally {
-			try {
-				in.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-				return "ERROR";
-			}
-			if (error != null) {
-				return "ERROR";
-			}
-		}
-
-		Sheet sheet = wb.getSheetAt(0);
-
-		// 行が見つからなくなるか、項目値が取得できるまで繰り返す
-		for (String postNumber : postNumberList) {
-			for (Row row : sheet) {
-				// A列と郵便番号を比較し、F列を取得
-				if (row.getCell(0).getStringCellValue().equals(postNumber)) {
-					switch (row.getCell(5).getCachedFormulaResultTypeEnum()) {
-					case NUMERIC:
-						return String.valueOf((int) row.getCell(5).getNumericCellValue());
-					case STRING:
-						return row.getCell(5).getStringCellValue();
-					default:
-						return null;
-					}
+			InputStream in = this.getClass().getResourceAsStream("/file/佐川コード突き当て.csv");
+			// ★SagawaCodeDtoをCommonLibsに用意してから、
+			CsvMapper mapper = new CsvMapper();
+			CsvSchema schema = CsvSchema.emptySchema().withHeader();
+			CsvSchema quoteSchema = mapper.schemaFor(SagawaCodeDto.class).withoutQuoteChar();
+			MappingIterator<SagawaCodeDto> it = mapper.readerFor(SagawaCodeDto.class).with(schema).with(quoteSchema).readValues(new InputStreamReader(in, "UTF-8"));
+			List<SagawaCodeDto> csvlist = it.readAll();
+			// CSVから取得した項目値と 設置先情報#郵便番号(手入力)、設置先情報#郵便番号の順で比較
+			for (String postNumber : postNumberList) {
+				List<SagawaCodeDto> SagawaCodeDtoList = csvlist.stream().filter(SagawaCodeDto -> SagawaCodeDto.getPostNumber().equals(postNumber)).collect(Collectors.toList());
+				if (CollectionUtils.isNotEmpty(SagawaCodeDtoList) && StringUtils.isNotBlank(SagawaCodeDtoList.get(0).getHp())) {
+					return SagawaCodeDtoList.get(0).getHp();
 				}
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ErrorCheckException(checkUtil.addErrorInfo(new ArrayList<ErrorInfo>(), "FileReadError"), e);
 		}
 		return null;
 	}
