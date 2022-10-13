acaa0109d7138b23dc447db366c3e1b915bd659a
tkamada <tatsuya.kamada@jrits.ricoh.co.jp>
2022-10-13 14:25:05 +0900

    SQL更新時更新者、更新日時設定 追加修正

--- Order/CreateOrderCsv/src/main/resources/sql/updateExtendsParameter.sql
+++ Order/CreateOrderCsv/src/main/resources/sql/updateExtendsParameter.sql
@@ -1,5 +1,5 @@
 UPDATE contract_detail detail 
 SET detail.extends_parameter = :extendsParam,
-    updated_user_id = 'COTOS_BATCH_USER',
-    updated_at = sysdate
+    detail.updated_user_id = 'COTOS_BATCH_USER',
+    detail.updated_at = sysdate
 WHERE detail.id IN (:idList)
